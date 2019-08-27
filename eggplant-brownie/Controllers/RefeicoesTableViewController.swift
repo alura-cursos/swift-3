731b3cec3f104d029a35584c9a788c38e3d52e77
Andriu <andriu.coelho@mercadolivre.com>
2019-08-27 15:02:32 -0300

    #Aula02

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -40,7 +40,11 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
             guard let indexPath = tableView.indexPath(for: celula) else { return }
             let refeicao = refeicoes[indexPath.row]
             
-            print("refeicao: \(refeicao.nome)")
+            let alerta = UIAlertController(title: refeicao.nome, message: refeicao.detalhes(), preferredStyle: .alert)
+            let botaoCancelar = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+            alerta.addAction(botaoCancelar)
+            
+            present(alerta, animated: true, completion: nil)
         }
     }
     
--- eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/Models/Refeicao.swift
@@ -35,4 +35,14 @@ class Refeicao: NSObject {
         
         return total
     }
+    
+    func detalhes() -> String {
+        var mensagem = "Felicidade: \(felicidade)"
+        
+        for item in itens {
+            mensagem += ", \(item.nome) - calorias: \(item.calorias)"
+        }
+        
+        return mensagem
+    }
 }
